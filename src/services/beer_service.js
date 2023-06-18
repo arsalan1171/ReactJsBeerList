3937c63a4a168cdcfea0a0abceaef103eae75aef
arsalan665@gmail.com <arsalan665@gmail.com>
2023-06-18 11:10:47 +0200

    change url to localhost

--- src/services/beer_service.js
+++ src/services/beer_service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-let API_URL = "https://beerservice.azurewebsites.net";
+let API_URL = "https://localhost:7199"; //"https://beerservice.azurewebsites.net";
 
 const getBeersList = async () => {
   try {
